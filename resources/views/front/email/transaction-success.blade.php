74413a88c48e522a25471979f6955f3c4fd5ee33
fajarrohimudin <fajarrohimudin@gmail.com>
2025-07-27 13:04:16 +0700

    fix

--- resources/views/front/email/transaction-success.blade.php
+++ resources/views/front/email/transaction-success.blade.php
@@ -1,16 +1,243 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+
 <head>
-    <title>Pembayaran Sukses</title>
+    <title></title>
+    <!--[if !mso]><!-- -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!--<![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <style type="text/css">
+        #outlook a {
+            padding: 0;
+        }
+        
+        .ReadMsgBody {
+            width: 100%;
+        }
+        
+        .ExternalClass {
+            width: 100%;
+        }
+        
+        .ExternalClass * {
+            line-height: 100%;
+        }
+        
+        body {
+            margin: 0;
+            padding: 0;
+            -webkit-text-size-adjust: 100%;
+            -ms-text-size-adjust: 100%;
+        }
+        
+        table,
+        td {
+            border-collapse: collapse;
+            mso-table-lspace: 0pt;
+            mso-table-rspace: 0pt;
+        }
+        
+        img {
+            border: 0;
+            height: auto;
+            line-height: 100%;
+            outline: none;
+            text-decoration: none;
+            -ms-interpolation-mode: bicubic;
+        }
+        
+        p {
+            display: block;
+            margin: 13px 0;
+        }
+    </style>
+    <!--[if !mso]><!-->
+    <style type="text/css">
+        @media only screen and (max-width:480px) {
+            @-ms-viewport {
+                width: 320px;
+            }
+            @viewport {
+                width: 320px;
+            }
+        }
+    </style>
+    <!--<![endif]-->
+    <!--[if mso]>
+        <xml>
+        <o:OfficeDocumentSettings>
+          <o:AllowPNG/>
+          <o:PixelsPerInch>96</o:PixelsPerInch>
+        </o:OfficeDocumentSettings>
+        </xml>
+        <![endif]-->
+    <!--[if lte mso 11]>
+        <style type="text/css">
+          .outlook-group-fix { width:100% !important; }
+        </style>
+        <![endif]-->
+    <!--[if !mso]><!-->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700" rel="stylesheet" type="text/css">
+    <style type="text/css">
+        @import url(https://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700);
+    </style>
+    <!--<![endif]-->
+    <style type="text/css">
+        @media only screen and (min-width:480px) {
+            .mj-column-per-100 {
+                width: 100% !important;
+                max-width: 100%;
+            }
+        }
+    </style>
+    <style type="text/css">
+        @media only screen and (max-width:480px) {
+            table.full-width-mobile {
+                width: 100% !important;
+            }
+            td.full-width-mobile {
+                width: auto !important;
+            }
+        }
+    </style>
 </head>
-<body>
-    <h2>Halo {{ $data->user->name }},</h2>
-    <p>Terima kasih, pembayaran Anda untuk transaksi <strong>{{ $data->trx_id }}</strong> telah berhasil.</p>
-    <p>Detail Transaksi Pesanan:</p>
-    <ul>
-        <li>Nama barang: {{ $data->product->name }}</li>
-        <li>Status pembayaran: {{ $data->transaction_status }}</li>
-        <li>Total: Rp {{ number_format($data->total_amount) }}</li>
-    </ul>
+
+<body style="background-color:#E4E6EB;">
+    <div style="background-color:#E4E6EB;">
+        <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="background:#FFFFFF;background-color:#FFFFFF;Margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#FFFFFF;background-color:#FFFFFF;width:100%;">
+                <tbody>
+                    <tr>
+                        <td style="border-bottom:1px dashed #eee;direction:ltr;font-size:0px;padding:20px 0;padding-bottom:0px;padding-top:0;text-align:center;vertical-align:top;">
+                            <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                            <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                                    <tr>
+                                        <td align="center" style="font-size:0px;padding:10px 25px;padding-top:30px;padding-right:0px;padding-bottom:25px;padding-left:0px;word-break:break-word;">
+                                            <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="border-collapse:collapse;border-spacing:0px;">
+                                                <tbody>
+                                                    <tr>
+                                                        <td style="width:60px;">
+                                                            <img alt="Logo" height="auto" src="{{ asset('assets/images/logos/logo.svg') }}" style="border:none;display:block;outline:none;text-decoration:none;height:auto;width:100%;" width="180">
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <!--[if mso | IE]></td></tr></table><![endif]-->
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="background:#FFFFFF;background-color:#FFFFFF;Margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#FFFFFF;background-color:#FFFFFF;width:100%;">
+                <tbody>
+                    <tr>
+                        <td style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:0px;padding-top:0;text-align:center;vertical-align:top;">
+                            <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
+                            <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%">
+                                    <tr>
+                                        <td align="center" style="font-size:0px;padding:10px 25px;padding-top:30px;padding-right:25px;padding-bottom:30px;padding-left:25px;word-break:break-word;">
+                                            <div style="font-family:Playfair Display, Times New Roman, serif;font-size:30px;font-weight:bold;line-height:1;text-align:center;color:#071C4D;">Terima kasih atas pembayaran Anda</div>
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <!--[if mso | IE]></td></tr></table><![endif]-->
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
+        <div style="background:#FFFFFF;background-color:#FFFFFF;Margin:0px auto;max-width:600px;">
+            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="background:#FFFFFF;background-color:#FFFFFF;width:100%;">
+                <tbody>
+                    <tr>
+                        <td style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:20px;padding-top:20px;text-align:center;vertical-align:top;">
+                            <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:middle;width:600px;" ><![endif]-->
+                            <div class="mj-column-per-100 outlook-group-fix" style="font-size:13px;text-align:left;direction:ltr;display:inline-block;vertical-align:middle;width:100%;">
+                                <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+                                    <tbody>
+                                        <tr>
+                                            <td style="border-bottom:1px dashed #eee;vertical-align:middle;padding-bottom:30px;">
+                                                <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
+                                                    <tr>
+                                                        <td align="left" style="font-size:0px;padding:10px 25px;padding-right:25px;padding-left:25px;word-break:break-word;">
+                                                            <div style="font-family:Assistant, Helvetica, Arial, sans-serif;font-size:18px;line-height:1;text-align:left;color:#071C4D;">Hi, {{ $data->user->name }}
+                                                                <br>
+                                                                <br>Pembayaran berhasil.
+                                                                <br>Kode transaksi <strong>#{{ $data->trx_id }}</strong></div>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td align="left" style="padding:10px 25px;">
+                                                            <table cellpadding="0" cellspacing="0" width="100%" border="0" style="color:#071C4D; font-family:Assistant, Helvetica, Arial, sans-serif; font-size:18px; line-height:28px; width:100%;">
+                                                                <tr>
+                                                                    <td style="padding-bottom:20px;">
+                                                                        <table cellpadding="0" cellspacing="0" width="100%" border="0" style="border-collapse: collapse; width:100%;">
+                                                                        <tr>
+                                                                            <td style="padding:5px 10px; font-weight:bold; width:40%;">Product</td>
+                                                                            <td style="padding:5px 10px;">{{ $data->product->name }}</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td style="padding:5px 10px; font-weight:bold;">Tanggal Sewa</td>
+                                                                            <td style="padding:5px 10px;">{{ $data->started_at }}</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td style="padding:5px 10px; font-weight:bold;">Durasi</td>
+                                                                            <td style="padding:5px 10px;">{{ $data->duration }} hari</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td style="padding:5px 10px; font-weight:bold;">Keterangan</td>
+                                                                            <td style="padding:5px 10px;">
+                                                                            @if($data->delivery_type == 'pickup')
+                                                                                Ambil di toko
+                                                                            @elseif($data->payment_method == 'home_delivery')
+                                                                                {{ $data->address }}
+                                                                            @else
+                                                                                -
+                                                                            @endif
+                                                                            </td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td style="padding:5px 10px; font-weight:bold;">Status pembayaran</td>
+                                                                            <td style="padding:5px 10px;">{{ $data->transaction_status }}</td>
+                                                                        </tr>
+                                                                        <tr>
+                                                                            <td style="padding:5px 10px; font-weight:bold;">Total</td>
+                                                                            <td style="padding:5px 10px;">Rp {{ number_format($data->total_amount) }}</td>
+                                                                        </tr>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <!--[if mso | IE]></td></tr></table><![endif]-->
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </div>
+        <!--[if mso | IE]></td></tr></table><![endif]-->
+    </div>
 </body>
+
 </html>
